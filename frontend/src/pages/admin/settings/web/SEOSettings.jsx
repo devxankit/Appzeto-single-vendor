f3aea98d16ca00b1dc00d23f1bdc29be10ad303a
harshu-panchal <harshvardhanpanc145@gmail.com>
2025-12-06 23:37:50 +0530

    admin side bar added again

--- frontend/src/pages/admin/settings/web/SEOSettings.jsx
+++ frontend/src/pages/admin/settings/web/SEOSettings.jsx
@@ -1,7 +1,7 @@
 import { useState } from 'react';
 import { FiSave, FiSearch } from 'react-icons/fi';
 import { motion } from 'framer-motion';
-import AnimatedSelect from '../../../components/Admin/AnimatedSelect';
+import AnimatedSelect from '../../../../components/Admin/AnimatedSelect';
 import toast from 'react-hot-toast';
 
 const SEOSettings = () => {
